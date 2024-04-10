39234377b55f85fd643024bea2b7b980ea1f7106
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-04-10 18:32:46 +0000

    docstring

--- rov_ooi/src/move_straight.py
+++ rov_ooi/src/move_straight.py
@@ -71,6 +71,21 @@ def spawn_object(model_name: str = "ooi", position: np.ndarray = np.zeros(3), ro
 
 
 def move_straight(model_name: str = "ooi", speed: float = 0.1, axis: str = "x"):
+    """
+    Moves a specified Gazebo model straight along the specified axis at a given speed.
+
+    Args:
+        model_name (str, optional): The name of the Gazebo model to be moved. Defaults to "ooi".
+        speed (float, optional): The speed at which the model moves along the specified axis. Defaults to 0.1.
+        axis (str, optional): The axis along which the model moves. Can be 'x', 'y', or 'z'. Defaults to "x".
+
+    Note:
+        This function publishes the model's new state to the '/gazebo/set_model_state' topic in the Gazebo environment.
+
+    Example:
+        To move a model named 'robot' along the y-axis at a speed of 0.2:
+        >>> move_straight(model_name="robot", speed=0.2, axis="y")
+    """
     rospy.init_node('move_straight', anonymous=True)
 
     rate = rospy.Rate(10)  # 10 Hz
@@ -98,8 +113,8 @@ if __name__ == '__main__':
     model_name = rospy.get_param('~model_name', "ooi")
     model_pos = rospy.get_param('~model_pos', [0, 0, 0])
     model_rot = rospy.get_param('~model_rot', [0, 0, 0, 1])
-    movement_axis = rospy.get_param('~move_axis', 0.1)
-    speed = rospy.get_param('~speed', 0.1)
+    movement_axis = rospy.get_param('~move_axis', 'x')
+    movement_speed = rospy.get_param('~speed', 0.1)
 
     try:
         # Spawn object
@@ -108,7 +123,7 @@ if __name__ == '__main__':
         print("Spawned object, now moving")
 
         # Move object
-        move_straight(model_name = model_name, speed = speed, axis = movement_axis)
+        move_straight(model_name = model_name, speed = movement_speed, axis = movement_axis)
 
     except rospy.ROSInterruptException:
         pass
