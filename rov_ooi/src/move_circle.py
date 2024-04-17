7522538e8f8c4b593c2ddae60f669137d7d3a2e7
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-04-17 10:03:47 +0000

    add depth param

--- rov_ooi/src/move_circle.py
+++ rov_ooi/src/move_circle.py
@@ -8,14 +8,15 @@ from gazebo_msgs.msg import ModelState
 
 from commons.spawn_object import spawn_object
 
-def move_circle(model_name: str, speed: float, radius: float):
+def move_circle(model_name: str, speed: float, radius: float, depth: float):
     """
     Moves a specified Gazebo model in a circle in the x,y-plane
 
     Args:
-        model_name (str, optional): The name of the Gazebo model to be moved
-        speed (float, optional): The speed at which the model moves along the specified axis
-        radius (float, optional): The radius of the circle to move in
+        model_name (str): The name of the Gazebo model to be moved
+        speed (float): The speed at which the model moves along the specified axis
+        radius (float): The radius of the circle to move in
+        depth (float): The depth at which to move in circles
 
     Note:
         This function publishes the model's new state to the '/gazebo/set_model_state' topic in the Gazebo environment.
@@ -30,6 +31,7 @@ def move_circle(model_name: str, speed: float, radius: float):
     pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=10)
     
     state_msg = ModelState(model_name, Pose(Point(), Quaternion()), Twist(), "world")
+    state_msg.pose.position.z = depth
 
     step = 0
     while not rospy.is_shutdown():
@@ -51,6 +53,7 @@ if __name__ == '__main__':
     model_rot = rospy.get_param('~model_rot', [0, 0, 0, 1])
     movement_radius = rospy.get_param('~move_radius', 50.0)
     movement_speed = rospy.get_param('~move_speed', 0.5)
+    depth = rospy.get_param('~depth', 0.5)
 
     try:
         # Spawn object
@@ -59,7 +62,7 @@ if __name__ == '__main__':
         print("Spawned object, now moving")
 
         # Move object
-        move_circle(model_name = model_name, speed = movement_speed, radius = movement_radius)
+        move_circle(model_name = model_name, speed = movement_speed, radius = movement_radius, depth = depth)
 
     except rospy.ROSInterruptException:
         pass
